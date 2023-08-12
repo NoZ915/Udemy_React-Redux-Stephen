0ef79941375fd3ed0c3aaa2f23aafc72ae6e7646
NoZ915 <tina0915tw0915@gmail.com>
2023-08-12 11:24:33 +0800

    finished: ~L77

--- 05-pics/src/App.js
+++ 05-pics/src/App.js
@@ -1,12 +1,19 @@
-import SearchBar from './components/SearchBar'
+
+import { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import searchImages from './api';
 
 function App(){
-    const handleClick = (term) => {
-        console.log("Do a search for "+term);
+    const [images, setImages] = useState([]);
+    const handleClick = async (term) => {
+        const result = await searchImages(term);
+        setImages(result);
     }
 
     return(<div>
         <SearchBar onSubmit = {handleClick}/>
+        <ImageList images={images}/>
     </div>);
 }
 
