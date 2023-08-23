0a6f8558cc54ca22095958b72eca5d4465b4ab24
NoZ915 <tina0915tw0915@gmail.com>
2023-08-23 18:29:07 +0800

    finished: ~L110

--- 06-books/src/App.js
+++ 06-books/src/App.js
@@ -4,6 +4,23 @@ import BookList from "./components/BookList";
 
 function App(){
     const [books, setBooks] = useState([]);
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+        setBooks(updatedBooks);
+    }
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if(book.id === id){
+                return {...book, title: newTitle};
+            }
+            return book;
+        })
+        setBooks(updatedBooks);
+    }
+
     const createBook = (title) => {
         const updatedBooks = [
             ...books,
@@ -17,7 +34,7 @@ function App(){
 
     return(
         <div className="app">
-            <BookList books={books}/>
+            <BookList onEdit={editBookById} books={books} onDelete={deleteBookById}/>
             <BookCreate onCreate={createBook}/>
         </div>
     )
