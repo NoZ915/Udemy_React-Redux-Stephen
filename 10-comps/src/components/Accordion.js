a172a606c43623b45e19eb098fb419227bbfb26b
NoZ915 <tina0915tw0915@gmail.com>
2023-09-28 12:17:44 +0800

    finished: ~L184

--- 10-comps/src/components/Accordion.js
+++ 10-comps/src/components/Accordion.js
@@ -3,14 +3,17 @@ import { useState } from "react";
 function Accordion({items}){
     const [expandedIndex, setExpandedIndex] = useState(0);
 
+    const handleClick = (nextIndex) => {
+        setExpandedIndex(nextIndex);
+    }
+
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
-        const content = isExpanded && <div>{item.content}</div>
 
         return(
             <div key={item.id}>
-                <div>{item.label}</div>
-                {content}
+                <div onClick={() => handleClick(index)}>{item.label}</div>
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     })
