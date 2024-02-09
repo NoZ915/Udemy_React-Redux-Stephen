0e0ed2f008659fedbeb56842d6ab0103332bb8a0
NoZ915 <tina0915tw0915@gmail.com>
2024-02-09 15:18:10 +0800

    finished: ~L280

--- 10-comps/src/components/SortableTable.js
+++ 10-comps/src/components/SortableTable.js
@@ -9,6 +9,12 @@ function SortableTable(props) {
     const { config, data } = props;
 
     const handleClick = (label) => {
+        if (sortBy && label !== sortBy) {
+            setSortOrder("asc");
+            setSortBy(label);
+            return;
+        }
+
         if (sortOrder === null) {
             setSortOrder("asc");
             setSortBy(label);
@@ -39,7 +45,7 @@ function SortableTable(props) {
     // 不能直接對prop或state修改 > copy data prop
     // 利用sortBy找到對的sortValue，並用它來幫助我們進行排序
     let sortedData = data;
-    if(sortOrder && sortBy){
+    if (sortOrder && sortBy) {
         // const column = config.find(column => column.label === sortBy);
         // 我們只要column中的sortValue，用解構方式取出
         const { sortValue } = config.find(column => column.label === sortBy);
@@ -49,50 +55,51 @@ function SortableTable(props) {
 
             const reverseOrder = sortOrder === "asc" ? 1 : -1;
 
-            if(typeof valueA === "string"){
+            if (typeof valueA === "string") {
                 return valueA.localeCompare(valueB) * reverseOrder;
-            }else{
+            } else {
                 return (valueA - valueB) * reverseOrder;
             }
         })
     }
 
     return (
-        <div>
-            {sortOrder} - {sortBy}
-            <Table {...props} data={sortedData} config={updatedConfig} />
-        </div>
+        <Table {...props} data={sortedData} config={updatedConfig} />
     )
 }
 
-function getIcons(label, sortBy, sortOrder){
-    if(label !== sortBy){
+function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy) {
         //同時出現上下箭頭
-        return(
+        return (
             <div>
-                <GoArrowDown />
-                <GoArrowUp />
+                <div>
+                    <GoArrowUp />
+                </div>
+                <div>
+                    <GoArrowDown />
+                </div>
             </div>
         )
     }
-    if(sortOrder === null){
+    if (sortOrder === null) {
         //同時出現上下箭頭
-        return(
+        return (
             <div>
+                <GoArrowUp />
                 <GoArrowDown />
-                <GoArrowUp />
             </div>
         )
-    }else if(sortOrder === "asc"){
+    } else if (sortOrder === "asc") {
         //出現向上箭頭
-        return(
+        return (
             <div>
                 <GoArrowUp />
             </div>
         )
-    }else if(sortOrder === "desc"){
+    } else if (sortOrder === "desc") {
         //出現向下箭頭
-        return(
+        return (
             <div>
                 <GoArrowDown />
             </div>
