2392f3c66cc0bf053d74e93ecba270391ec9d713
NoZ915 <tina0915tw0915@gmail.com>
2024-03-17 19:58:59 +0800

    finished: ~L402

--- 21-media/src/store/apis/albumsApi.js
+++ 21-media/src/store/apis/albumsApi.js
@@ -1,16 +1,39 @@
 import { faker } from "@faker-js/faker";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// DEV ONLY!!!
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
 const albumsApi = createApi({
     reducerPath: "albums",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3005"
+        baseUrl: "http://localhost:3005",
+        // REMOVE FOR PRODUCTION
+        fetchFn: async (...args) => {
+            await pause(1000);
+            return fetch(...args);
+        }
     }),
-    endpoints(builder){
-        return{
+    endpoints(builder) {
+        return {
+            removeAlbum: builder.mutation({
+                query: (album) => {
+                    return {
+                        url: `/albums/${album.id}`,
+                        method: "DELETE"
+                    }
+                }
+            }),
             addAlbum: builder.mutation({
+                invalidatesTags: (result, error, user) => {
+                    return [{ type: "Album", id: user.id }]
+                },
                 query: (user) => {
-                    return{
+                    return {
                         url: "/albums",
                         method: "POST",
                         body: {
@@ -21,6 +44,9 @@ const albumsApi = createApi({
                 }
             }),
             fetchAlbums: builder.query({
+                providesTags: (result, error, user) => {
+                    return [{ type: "Album", id: user.id }]
+                },
                 query: (user) => {
                     return {
                         url: "/albums",
@@ -35,5 +61,5 @@ const albumsApi = createApi({
     }
 })
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } = albumsApi;
 export { albumsApi };
